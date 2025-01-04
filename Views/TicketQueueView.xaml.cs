7f92b3d92ee51b76a14990a0a4669d7d38af24dc
unknown <tepescohuite@gmail.com>
2025-01-04 00:03:31 +0000

    Added more sorting options. Added Sort options context menu. Added filter options menu.

--- Views/TicketQueueView.xaml.cs
+++ Views/TicketQueueView.xaml.cs
@@ -1,15 +1,9 @@
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Input;
-using TicketeX_.Models;
-using TicketeX_.Utilities;
 using TicketeX_.ViewModels;
-using DataGridRow = System.Windows.Controls.DataGridRow;
+using SortDescription = System.ComponentModel.SortDescription;
 
 namespace TicketeX_.Views;
 
@@ -20,13 +14,54 @@ public partial class TicketQueueView
     {
         InitializeComponent();
         DefaultSortQueue();
+        QueueGrid.ItemsSource = TicketsViewSource.View;
     }
 
     private void DefaultSortQueue()
     {
         TicketsViewSource.SortDescriptions.Add(new SortDescription("DateTimeLastUpdated", ListSortDirection.Descending));
-        QueueGrid.ItemsSource = TicketsViewSource.View;
     }
     
+    private void Bold_Checked(object sender, RoutedEventArgs e)
+    {
+        FilterMenuButton.FontWeight = FontWeights.Bold;
+    }
+
+    private void Bold_Unchecked(object sender, RoutedEventArgs e)
+    {
+        FilterMenuButton.FontWeight = FontWeights.Normal;
+    }
+
+    private void OpenContextMenuOnLeftClick(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var contextMenu = button?.ContextMenu;
+        if (contextMenu == null) return;
+        contextMenu.DataContext = button.DataContext;
+        contextMenu.IsOpen = true;
+        e.Handled = true;
+    }
+
+    private void SortSevLoFirst_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void SortSevHiFirst_OnClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void SortLastUpdatedAsc_OnClick(object sender, RoutedEventArgs routedEventArgs)
+    {
+        TicketsViewSource.SortDescriptions.Clear();
+        TicketsViewSource.SortDescriptions.Add(new SortDescription("DateTimeLastUpdated", ListSortDirection.Ascending));
+    }
+
+    private void SortLastUpdatedDesc_OnClick(object sender, RoutedEventArgs routedEventArgs)
+    {
+        TicketsViewSource.SortDescriptions.Clear();
+        TicketsViewSource.SortDescriptions.Add(new SortDescription("DateTimeLastUpdated", ListSortDirection.Descending));
+    }
 }
 
